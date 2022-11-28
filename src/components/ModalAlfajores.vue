1b33aa88f09b19131a57697e827e7519c4f3db32
lexpin23 <lesly_jpf_jnp@hotmail.com>
2022-11-27 23:48:49 -0700

    Componente ModalAlfajores
    Se agrega un dialog, donde se muestra un pequeño formulario que el cliente seleccionará a su gusto

--- src/components/ModalAlfajores.vue
+++ src/components/ModalAlfajores.vue
@@ -1,85 +1,125 @@
 <template>
     <div>
         <v-row justify="center">
-        <v-dialog v-model="dialog" persistent max-width="600px">
-            <template v-slot:activator="{ on, attrs }">
-                <v-btn dark v-bind="attrs" v-on="on">
-                    <v-icon>mdi-plus</v-icon>
-                </v-btn>
-                
-            </template>
-            <v-card>
-                <v-card-title>
-                    <span class="text-h5">{{title}}</span>
-                </v-card-title>
-                <v-card-text>
-                    <v-container>
-                        <v-row>
-                            <v-col cols="6" sm="3" md="4">
-                                <v-label>Cajas</v-label>
+            <v-dialog v-model="dialog" persistent max-width="400px" max-height="450px">
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn dark v-bind="attrs" v-on="on">
+                        <v-icon>mdi-plus</v-icon>
+                    </v-btn>
+
+                </template>
+                <v-card>
+                    <v-card-title class="grey lighten-2">
+                        <span class="text-h5">{{ title }}</span>
+                    </v-card-title>
+                    <v-card-text>
+                        <v-container>
+                            <v-col>
+                                <v-row>
+                                    <v-row class="mt-5">
+                                        <h2 class="mr-3">Cajas</h2>
+                                    </v-row>
+                                   
+                                    <v-row>
+                                        <v-btn type="button" v-if="counter < 0 ? counter = 0 : 'logrado'" v-on:click="counter -= 1">-</v-btn>
+                                        <p class="ml-2 mr-2 mt-2">{{ counter }}</p>
+                                        <v-btn v-on:click="counter += 1" class="mr-auto">+</v-btn>
+                                    </v-row>
+                                    
+                                </v-row>
+
+                                <v-row class="mt-5">
+                                    <v-row class="mt-5">
+                                        <h2 class="mr-3">Toppings</h2>
+                                    </v-row>
+                                </v-row>
+
+                                <v-row>
+                                    <v-row class="mt-5">
+                                            <v-combobox v-model="select" :items="items" label="Seleccionar Topping" multiple outlined dense></v-combobox>
+                                    </v-row>
+                                </v-row>
                                 
-                            </v-col>
-                            <v-col cols="6" sm="3" md="4">
-                                <v-label>Toppings</v-label>
-                            </v-col>
-                        </v-row>
-                        <v-row>
-                            <v-col cols="6" sm="3" md="6">
-                                <v-row>
-                                    <v-btn v-on:click="counter -= 1">-</v-btn>
-                                        <p>{{ counter }}</p>
-                                    <v-btn v-on:click="counter += 1">+</v-btn>
+
+                                <v-row class="mt-3">
+                                    <v-row class="mt-5">
+                                        <h2>Total</h2>     
+                                    </v-row>
+                                </v-row>
+
+                                <v-row class="mt-3">
+                                    <v-row class="mt-5"><v-label>${{ (cajaA * counter) + (counterT * coco) + (counterA * azucarG) }}</v-label></v-row>
                                 </v-row>
                             </v-col>
-                            <v-col cols="6" sm="3" md="6">
-                                <v-row>
-                                    <v-label>Coco</v-label>
-                                    <v-btn v-on:click="counterT -= 1">-</v-btn>
-                                        <p>{{ counterT }}</p>
-                                    <v-btn v-on:click="counterT += 1">+</v-btn>
-                                </v-row>
-                                <v-row>
-                                    <v-label>Azúcar glass</v-label>
-                                    <v-btn v-on:click="counterA -= 1">-</v-btn>
-                                        <p>{{ counterA }}</p>
-                                    <v-btn v-on:click="counterA += 1">+</v-btn>
-                                </v-row>
-                            </v-col>
-                        </v-row>
-                        <v-row>
-                            <v-col cols="12" sm="6" md="4">
-                                <v-label>Total</v-label>
-                            </v-col>
-                        </v-row>
-                    </v-container>
-                </v-card-text>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="blue darken-1" text @click="dialog = false">
-                        Cerrar
-                    </v-btn>
-                    <v-btn color="blue darken-1" text @click="dialog = false">
-                        Agregar al carrito
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
-    </v-row>
+
+                        </v-container>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue darken-1" text @click="dialog = false">
+                            Cerrar
+                        </v-btn>
+                        <v-btn color="blue darken-1" text @click="agregarCarrito()">
+                            Agregar al carrito
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+        </v-row>
     </div>
 </template>
 
 <script>
 
-  export default {
-    props:['title'],
+export default {
+    props: ['title'],
 
     data: () => ({
-      dialog: false,
-      counter: 0,
-      counterT: 0,
-      counterA: 0
+        dialog: false,
+        counter: 1,
+        counterT: 0,
+        counterA: 0,
+        cajaA: 80,
+        coco: 10,
+        azucarG: 5,
+        desbtn: false,
+        desbtnT: false,
+        select: [],
+        items: [
+          'Coco',
+          'Azúcar Glass'
+        ],
     }),
 
-    
-  }
+    methods: {
+        agregarCarrito() {
+
+            if (this.counter > 0) {
+                alert('Se añadió al carrito')
+                alert(this.counter)
+
+                //Establecer valores en 0
+                this.counter = 0
+                this.counterT = 0
+                this.counterA = 0
+
+                this.dialog = false
+            }
+            else {
+
+            }
+        }
+    },
+    watch: {
+
+    }
+
+
+}
 </script>
+
+<style>
+#v-card-title {
+    background-color: #ffffff9d;
+}
+</style>
