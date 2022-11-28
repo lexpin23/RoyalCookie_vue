5ddaa0169053ad49aba83277635eef3fda8559cc
lexpin23 <lesly_jpf_jnp@hotmail.com>
2022-11-27 23:50:30 -0700

    Componente ModalCarrito
    - Se agrega el botón carrito al nav
    
    - Creamos la vista del carrito siendo de igua forma un dialog, con algunas líneas de pedido muestra, para apreciar cómo se vera.

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,60 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="#fd79a8"
-      dark
-    >
+    <v-app-bar app color="#fd79a8" dark>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="../src/assets/RoyalCookieLogo2.png"
-          transition="scale-transition"
-          width="40"
-        />
+        <v-img alt="Vuetify Logo" class="shrink mr-2" contain src="../src/assets/RoyalCookieLogo2.png"
+          transition="scale-transition" width="40" />
 
-      <h2>Royal Cookie</h2>
+        <h2>Royal Cookie</h2>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        v-for = "(item, index) in rutas" :key = "index"
-        text
-        @click="insertarRuta(item.ruta)"
-      >
-        <span class="mr-2">{{item.nombre}}</span>
+      <v-btn v-for="(item, index) in rutas" :key="index" text @click="insertarRuta(item.ruta)">
+        <span class="mr-2">{{ item.nombre }}</span>
       </v-btn>
 
+          <ModalCarrito></ModalCarrito>
+       
     </v-app-bar>
 
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
 
+import ModalCarrito from './components/ModalCarrito.vue';
+
+
 export default {
+
   name: 'App',
 
-  data: () => ({
-    rutas: [
-      {nombre: 'Home', ruta: '/'},
-      {nombre: 'About', ruta: '/about'},
-      {nombre: 'Catalogo', ruta: '/catalogo'},
-      {nombre: 'Pedidos', ruta: '/pedidos'},
-    ]
-  }),
+  components: {
+    ModalCarrito
+
+  },
+
+  data() {
+    return {
+
+      rutas: [
+        { nombre: 'Home', ruta: '/' },
+        { nombre: 'About', ruta: '/about' },
+        { nombre: 'Catalogo', ruta: '/catalogo' },
+        { nombre: 'Pedidos', ruta: '/pedidos' },
+      ],
+      ModalCarrito: false,
+
+    }
+
+  },
 
   methods: {
-    insertarRuta(ruta){
+    insertarRuta(ruta) {
       this.$router.push(ruta)
     }
   }
---
+++ src/components/ModalCarrito.vue
@@ -0,0 +1,101 @@
+<template>
+
+    <div class="text-center">
+        <v-dialog v-model="dialog" width="700">
+            <template v-slot:activator="{ on, attrs }">
+                <v-btn dark v-bind="attrs" v-on="on">
+                    <v-icon>mdi-cart</v-icon>
+                </v-btn>
+            </template>
+
+            <v-card>
+                <v-card-title class="text-h5 grey lighten-2">
+                    Carrito
+                </v-card-title>
+
+                <v-simple-table>
+                    <template v-slot:default>
+                        <thead>
+                            <tr>
+                                <th class="text-left">
+                                    ID
+                                </th>
+                                <th class="text-left">
+                                    Producto
+                                </th>
+                                <th class="text-left">
+                                    Toppings
+                                </th>
+                                <th class="text-left">
+                                    Cantidad
+                                </th>
+                                <th class="text-left">
+                                    Precio
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="item in desserts" :key="item.name">
+                                <td>{{ item.id }}</td>
+                                <td>{{ item.producto }}</td>
+                                <td>{{ item.toppings }}</td>
+                                <td>{{ item.cantidad }}</td>
+                                <td>{{ item.precio }}</td>
+                            </tr>
+                        </tbody>
+                    </template>
+                </v-simple-table>
+
+                <v-divider></v-divider>
+
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="dialog = false">
+                        Cerrar
+                    </v-btn>
+                    <v-btn color="red darken-2" text @click="dialog = false">
+                        Pagar
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+
+    name: 'carrito',
+    data() {
+        return {
+            dialog: false,
+            desserts: [
+          {
+            id: 1,
+            producto: 'Alfajores',
+            toppings: 'Coco',
+            cantidad: 1,
+            precio: 80
+          },
+          {
+            id: 2,
+            producto: 'Bollitos',
+            toppings: '',
+            cantidad: 2,
+            precio: 100
+          },
+          {
+            id: 3,
+            producto: 'Pastel Chocolate',
+            toppings: '',
+            cantidad: 1,
+            precio: 150
+          },
+        ],
+        }
+    },
+
+}
+</script>
