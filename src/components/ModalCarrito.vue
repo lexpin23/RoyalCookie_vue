165a737b14e0c9e6d5c90895bb08fcf88c120a8a
lexpin23 <lesly_jpf_jnp@hotmail.com>
2022-12-07 03:05:35 -0700

    Añadiendo Pedidos y vista Inicio

--- src/components/ModalCarrito.vue
+++ src/components/ModalCarrito.vue
@@ -11,75 +11,44 @@
                     Carrito
             </v-card-title>
 
-            <v-data-table :headers="headers" :items="desserts" class="elevation-1">
-            <template v-slot:top>
-              
-                    <v-dialog v-model="dialog" max-width="500px">
+            <v-data-table :headers="headers" :items="desserts">
+                <template v-slot:top>
+                        <v-dialog v-model="dialogDelete" max-width="500px">
+                            <v-card>
+                                <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+                                <v-card-actions>
+                                    <v-spacer></v-spacer>
+                                    <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                                    <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                                    <v-spacer></v-spacer>
+                                </v-card-actions>
+                            </v-card>
+                        </v-dialog>
+                    
+                </template>
 
-                        <v-card>
-                            <v-card-text>
-                                <v-container>
-                                    <v-row>
-                                        <v-col cols="12" sm="6" md="4">
-                                            <v-text-field v-model="editedItem.producto" label="ID"></v-text-field>
-                                        </v-col>
-                                        <v-col cols="12" sm="6" md="4">
-                                            <v-text-field v-model="editedItem.descripcion" label="Descripcion"></v-text-field>
-                                        </v-col>
-                                        <v-col cols="12" sm="6" md="4">
-                                            <v-text-field v-model="editedItem.toppings" label="Toppings"></v-text-field>
-                                        </v-col>
-                                        <v-col cols="12" sm="6" md="4">
-                                            <v-text-field v-model="editedItem.cantidad" label="Cantidad"></v-text-field>
-                                        </v-col>
-                                        <v-col cols="12" sm="6" md="4">
-                                            <v-text-field v-model="editedItem.precio" label="Precio"></v-text-field>
-                                        </v-col>
-                                    </v-row>
-                                </v-container>
-                            </v-card-text>
+                <template v-slot:[`item.actions`]="{ item }">
+                    <v-icon small class="mr-2" @click="editItem(item)">
+                        mdi-pencil
+                    </v-icon>
+                    <v-icon small @click="deleteItem(item)">
+                        mdi-delete
+                    </v-icon>
+                </template>
 
-                            <v-card-actions>
-                                <v-spacer></v-spacer>
-                                <v-btn color="blue darken-1" text @click="close">
-                                    Cancel
-                                </v-btn>
-                                <v-btn color="blue darken-1" text @click="save">
-                                    Save
-                                </v-btn>
-                            </v-card-actions>
-                        </v-card>
-                    </v-dialog>
-                    <v-dialog v-model="dialogDelete" max-width="500px">
-                        <v-card>
-                            <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
-                            <v-card-actions>
-                                <v-spacer></v-spacer>
-                                <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-                                <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
-                                <v-spacer></v-spacer>
-                            </v-card-actions>
-                        </v-card>
-                    </v-dialog>
-                
-            </template>
-            <template v-slot:item.actions="{ item }">
-                <v-icon small class="mr-2" @click="editItem(item)">
-                    mdi-pencil
-                </v-icon>
-                <v-icon small @click="deleteItem(item)">
-                    mdi-delete
-                </v-icon>
-            </template>
-            <template v-slot:no-data>
-                <v-btn color="primary" @click="initialize">
-                    Reset
+            </v-data-table>
+
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="dialog = false">
+                    Cerrar
                 </v-btn>
-            </template>
-        </v-data-table>
-        </v-card>
+                <v-btn color="blue darken-1" text class="mr-4" >
+                    Pagar
+                </v-btn>
+            </v-card-actions>
 
-        
+        </v-card>        
     </v-dialog>
 
 </template>
@@ -124,15 +93,9 @@ export default {
     }),
 
     computed: {
-        formTitle() {
-            return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
-        },
     },
 
     watch: {
-        dialog(val) {
-            val || this.close()
-        },
         dialogDelete(val) {
             val || this.closeDelete()
         },
@@ -173,9 +136,10 @@ export default {
         },
 
         editItem(item) {
-            this.editedIndex = this.desserts.indexOf(item)
             this.editedItem = Object.assign({}, item)
-            this.dialog = true
+            console.log( this.editedItem)
+            console.log( this.editedItem.cantidad )
+            
         },
 
         deleteItem(item) {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,3 +13,5 @@
     },
   }
 </script>
+
+<style></style>
