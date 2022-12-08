ec97bd4204181634012a5e11f57a3fe853bbe825
lexpin23 <lesly_jpf_jnp@hotmail.com>
2022-12-08 00:35:28 -0700

    Agregando axios

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
@@ -10,5 +11,6 @@ new Vue({
   router,
   store,
   vuetify,
+  axios,
   render: function (h) { return h(App) }
 }).$mount('#app')
